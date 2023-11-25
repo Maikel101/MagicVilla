761da4ac3d786340d9cf070a867582cffb68fed7
Maikel101 <miguel.rol@outlook.es>
2023-11-25 12:11:08 +0100

    DbContext - Inyeccion de Dependencia

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,48 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        //Tablas de la base de datos
+        public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa()
+                {
+                    Id= 1,
+                    Nombre="Villa Real",
+                    Detalle="Detalle de la Villa...",
+                    ImagenUrl="",
+                    Ocupantes=5,
+                    MetrosCuadrados=50,
+                    Tarifa=200,
+                    Amenidad="",
+                    FechaCreacion=DateTime.Now,
+                    FechaAcualizacion=DateTime.Now
+                },
+                  new Villa()
+                  {
+                      Id = 2,
+                      Nombre = "Premium vista a la Playa",
+                      Detalle = "Detalle de la Villa...",
+                      ImagenUrl = "",
+                      Ocupantes = 6,
+                      MetrosCuadrados = 80,
+                      Tarifa = 250,
+                      Amenidad = "",
+                      FechaCreacion = DateTime.Now,
+                      FechaAcualizacion = DateTime.Now
+                  }
+          );
+        }
+    }
+}
---
+++ MagicVilla_API/Migrations/20231124202533_AlimentarTablaVilla.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MagicVillaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AlimentarTablaVilla : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenidad", "Detalle", "FechaAcualizacion", "FechaCreacion", "ImagenUrl", "MetrosCuadrados", "Nombre", "Ocupantes", "Tarifa" },
+                values: new object[,]
+                {
+                    { 1, "", "Detalle de la Villa...", new DateTime(2023, 11, 24, 21, 25, 33, 120, DateTimeKind.Local).AddTicks(5567), new DateTime(2023, 11, 24, 21, 25, 33, 120, DateTimeKind.Local).AddTicks(5518), "", 50, "Villa Real", 5, 200.0 },
+                    { 2, "", "Detalle de la Villa...", new DateTime(2023, 11, 24, 21, 25, 33, 120, DateTimeKind.Local).AddTicks(5576), new DateTime(2023, 11, 24, 21, 25, 33, 120, DateTimeKind.Local).AddTicks(5574), "", 80, "Premium vista a la Playa", 6, 250.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
